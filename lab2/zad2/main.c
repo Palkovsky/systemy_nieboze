af7228b2df3a79b068afd4251b2fcde4ac9d3a3a
dawid <dawidmacek42@gmail.com>
2019-03-14 17:33:47 +0100

    First part of 2nd exercise

---
+++ lab2/zad2/main.c
@@ -0,0 +1,169 @@
+#define _XOPEN_SOURCE
+#define _BSD_SOURCE
+
+#include <limits.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <errno.h>
+
+typedef struct dirent dirent;
+
+void printUsageAndExit(){
+  printf("Invalid parameters! USAGE:\n");
+  printf("main.out [directory] [<, =, >] [DD:MM:YYYY-HH:MM:SS]\n");
+  exit(1);
+}
+
+uint8_t parseOperator(const char* operator){
+  if(strcmp(operator, ">") == 0) return 0;
+  if(strcmp(operator, "<") == 0) return 1;
+  if(strcmp(operator, "=") == 0) return 2;
+  printf("Invalid operation symbol.\n");
+  exit(3);
+}
+
+time_t parseDate(const char* datetime){
+  struct tm tm;
+  time_t timestamp;
+
+  if (strptime(datetime, "%d.%m.%Y-%H:%M:%S", &tm) == NULL){
+    printf("Invalid date format! Use: DD:MM:YYYY-HH:MM:SS\n");
+    exit(4);
+  }
+
+  timestamp = mktime(&tm);
+  if(timestamp < 0){
+    printf("Unable to convert date to epoch.\n");
+    exit(4);
+  }
+
+  return timestamp;
+}
+
+char* fileTypeToString(unsigned char id){
+  switch(id){
+  case DT_REG:
+    return "file";
+  case DT_DIR:
+    return "dir";
+  case DT_FIFO:
+    return "fifo";
+  case DT_SOCK:
+    return "sock";
+  case DT_CHR:
+    return "char dev";
+  case DT_BLK:
+    return "block dev";
+  case DT_LNK:
+    return "slink";
+ }
+  return "unknown";
+}
+
+int8_t shouldPrint(time_t op1, uint8_t operator, time_t op2){
+  if(operator == 0){ // >
+    if(op1 > op2) return 1;
+  } else if(operator == 1){ // <
+    if (op1 < op2) return 1;
+  } else if(operator == 2){ // =
+    if (op1 == op2) return 1;
+  }
+  return 0;
+}
+
+char *timestampToString(time_t time){
+  char *buff = calloc(50, sizeof(char));
+  strftime(buff, 50, "%d.%m.%Y-%H:%M:%S", localtime(&time));
+  return buff;
+}
+
+void traverseDirectory(const char* path, uint8_t operator, time_t timestamp){
+  dirent *entry = NULL;
+
+  DIR* dir = opendir(path);
+  if(!dir){
+    //printf("Invalid directory path!\n");
+    //exit(2);
+    return;
+  }
+
+  struct stat fileinfo;
+
+  while((entry = readdir(dir)) != NULL){
+    if(strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+      continue;
+
+    char *absolute_path;
+    char *file_type;
+    uint64_t file_size;
+    time_t last_access;
+    time_t last_update;
+
+    char *filename = entry->d_name;
+    char *newPath = calloc(PATH_MAX + 1, sizeof(char));
+    sprintf(newPath, "%s/%s", path, filename);
+    absolute_path = (char *) realpath(newPath, NULL);
+
+    unsigned char f_type_id = entry->d_type;
+    file_type = fileTypeToString(f_type_id);
+
+    if(stat(absolute_path, &fileinfo) < 0){
+      printf("ERROR: %s\n", strerror(errno));
+      free(newPath);
+      free(absolute_path);
+      continue;
+    }
+
+    file_size = fileinfo.st_size;
+    last_access = fileinfo.st_atime;
+    last_update = fileinfo.st_mtime;
+    char* last_access_str = timestampToString(last_access);
+    char* last_update_str = timestampToString(last_update);
+
+    if(shouldPrint(last_update, operator, timestamp) == 1){
+      printf("%s | %s | %lu bytes | %s | %s\n",
+             absolute_path,
+             file_type,
+             file_size,
+             last_access_str,
+             last_update_str);
+    }
+
+    if(f_type_id == DT_DIR && f_type_id != DT_LNK){
+      traverseDirectory(newPath, operator, timestamp);
+    }
+
+    free(newPath);
+    free(absolute_path);
+    free(last_access_str);
+    free(last_update_str);
+  }
+
+  closedir(dir);
+
+  return;
+}
+
+int main(int argc, char **argv){
+  if (argc != 4) printUsageAndExit();
+
+
+  uint32_t len = strlen(argv[1]);
+  if(argv[1][len-1] == '/'){
+    argv[1][len-1] = 0;
+  }
+
+  const char *path = argv[1];
+  uint8_t operator = parseOperator(argv[2]);
+  time_t timestamp = parseDate(argv[3]);
+
+  traverseDirectory(path, operator, timestamp);
+
+  return 0;
+}
