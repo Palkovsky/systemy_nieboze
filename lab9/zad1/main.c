a366dd4d07e512c445abeaaaa195653abd0f2df7
dawid <dawidmacek42@gmail.com>
2019-05-26 20:39:10 +0200

    Only mutexes/cond. Stop condition doesn't work.

--- lab9/zad1/main.c
+++ lab9/zad1/main.c
@@ -11,32 +11,40 @@
 /*
  * Helper struct to prevent global variables.
  */
-#define MUTEX_COUNT 5
+#define MUTEX_COUNT 10
+
 #define MUTEX_ENTER 0
 #define MUTEX_BUTTON 1
 #define MUTEX_LEAVE 2
 #define MUTEX_RIDE 3
+#define MUTEX_CART_AT_BAY 4
+#define MUTEX_WAIT_FOR_BUTTON 5
+#define MUTEX_QUEUE_FRONT 6
+#define MUTEX_WAIT_FOR_PASSENGERS 7
+#define MUTEX_RIDE_FINISH 8
+#define MUTEX_RIDE_START 9
 
 typedef struct T_Arg {
-    int identifier;
-    int rides_target;
-    int passenger_cap;
-    Cart **cart;
-    Cart *cart_head;
-    Passenger **passenger;
-    Passenger *passenger_head;
-    pthread_mutex_t *mutex[MUTEX_COUNT];
-    pthread_cond_t *cond[MUTEX_COUNT];
+  int identifier;
+  int rides_target;
+  int passenger_cap;
+  int cart_count;
+  int passenger_count;
+  Cart **cart;
+  Cart *cart_head;
+  Passenger **passenger;
+  Passenger *passenger_head;
+  pthread_mutex_t *mutex[MUTEX_COUNT];
+  pthread_cond_t *cond[MUTEX_COUNT];
+  pthread_t *cart_tids;
+  pthread_t *passenger_tids;
 } T_Arg;
-
 
 /*
  * Thread handlers.
  */
 void *cart_routine(void*);
 void *passenger_routine(void*);
-void cart_test_stop_cond(T_Arg*);
-void passenger_test_stop_cond(T_Arg*);
 
 void spawn_threads(pthread_t*, int, void* (*) (void*), T_Arg*);
 void join_threads(pthread_t*, int);
@@ -89,6 +97,8 @@ int main(int argc, char **argv)
   targ = malloc(sizeof(T_Arg));
   targ->rides_target = rides_count;
   targ->passenger_cap = cart_capacity;
+  targ->cart_count = cart_thread_count;
+  targ->passenger_count = passenger_thread_count;
   targ->cart = &cart;
   targ->cart_head = cart_head;
   targ->passenger = &passenger;
@@ -104,22 +114,23 @@ int main(int argc, char **argv)
 
   cart_tids = malloc(sizeof(pthread_t) * cart_thread_count);
   passenger_tids = malloc(sizeof(pthread_t) * passenger_thread_count);
+  targ->cart_tids = cart_tids;
+  targ->passenger_tids = passenger_tids;
 
   spawn_threads(cart_tids, cart_thread_count, cart_routine, targ);
   spawn_threads(passenger_tids, passenger_thread_count, passenger_routine, targ);
-  
+
   join_threads(cart_tids, cart_thread_count);
   join_threads(passenger_tids, passenger_thread_count);
-  
+
   free(passenger_tids);
   free(cart_tids);
   free(cart_head);
   free(passenger_head);
   free(targ);
-  
+
   return 0;
 }
-
 
 /*
  * Threads logic
@@ -137,70 +148,91 @@ void *cart_routine(void* arg)
     this_cart = cart_head + this_id;
 
     /* While last cart haven't done specific number of rides*/
-    while(cart_head->prev->rides_done < targ->rides_target) 
+    while(cart_head->prev->rides_done < targ->rides_target)
     {
          /* While not in place for accepting passegners*/
-        while(*targ->cart != this_cart){}
+      pthread_mutex_lock(targ->mutex[MUTEX_CART_AT_BAY]);
+      while(*targ->cart != this_cart)
+      {
+        pthread_cond_wait(targ->cond[MUTEX_CART_AT_BAY], targ->mutex[MUTEX_CART_AT_BAY]);
+      }
+      pthread_mutex_unlock(targ->mutex[MUTEX_CART_AT_BAY]);
 
-        cart_test_stop_cond(targ);
+      sprintf(message, "C%d: opening doors\n", this_id);
+      print_timestamped(message);
 
-        sprintf(message, "C%d: opening doors\n", this_id);
-        print_timestamped(message);
-        
-        this_cart->cart_state = STATE_ACCEPTING;
-        
-         /* While not full */
-        pthread_mutex_lock(targ->mutex[MUTEX_ENTER]);
-        while(this_cart->passengers_inside < targ->passenger_cap){
-            pthread_cond_wait(targ->cond[MUTEX_ENTER], targ->mutex[MUTEX_ENTER]);
-            pthread_mutex_unlock(targ->mutex[MUTEX_ENTER]);
-        }
+      this_cart->cart_state = STATE_ACCEPTING;
+      pthread_cond_broadcast(targ->cond[MUTEX_WAIT_FOR_PASSENGERS]);
 
-        this_cart->cart_state = STATE_WAITING_FOR_START;
-        
-        /* Wait until start pressed */
-        while(this_cart->cart_state != STATE_STARTED) {}
+      /* While not full */
+      pthread_mutex_lock(targ->mutex[MUTEX_ENTER]);
+      while(this_cart->passengers_inside < targ->passenger_cap)
+      {
+          pthread_cond_wait(targ->cond[MUTEX_ENTER], targ->mutex[MUTEX_ENTER]);
+      }
+      pthread_mutex_unlock(targ->mutex[MUTEX_ENTER]);
 
-        sprintf(message, "C%d: closing doors\n", this_id);
-        print_timestamped(message);
-        sprintf(message, "C%d: starting ride\n", this_id);
-        print_timestamped(message);
-        
-        this_cart->cart_state = STATE_RIDING;
-        *targ->cart = (*targ->cart)->next;
-                
-        sleep(1);
+      this_cart->cart_state = STATE_WAITING_FOR_START;
+      pthread_cond_broadcast(targ->cond[MUTEX_RIDE_START]);
+
+      /* Wait until start pressed */
+      pthread_mutex_lock(targ->mutex[MUTEX_WAIT_FOR_BUTTON]);
+      while(this_cart->cart_state != STATE_STARTED)
+      {
+        pthread_cond_wait(targ->cond[MUTEX_WAIT_FOR_BUTTON], targ->mutex[MUTEX_WAIT_FOR_BUTTON]);
+      }
+      pthread_mutex_unlock(targ->mutex[MUTEX_WAIT_FOR_BUTTON]);
+
+      pthread_cond_broadcast(targ->cond[MUTEX_RIDE]);
+
+      sprintf(message, "C%d: closing doors\n", this_id);
+      print_timestamped(message);
+      sprintf(message, "C%d: starting ride\n", this_id);
+      print_timestamped(message);
+
+      this_cart->cart_state = STATE_RIDING;
+      *targ->cart = (*targ->cart)->next;
+      pthread_cond_broadcast(targ->cond[MUTEX_CART_AT_BAY]);
+
+      sleep(1);
 
         /* Make sure cart is positioned at the front */
-        while(*targ->cart != this_cart)
-            {cart_test_stop_cond(targ);}
+      pthread_mutex_lock(targ->mutex[MUTEX_CART_AT_BAY]);
+      while(*targ->cart != this_cart)
+      {
+        pthread_cond_wait(targ->cond[MUTEX_CART_AT_BAY], targ->mutex[MUTEX_CART_AT_BAY]);
+      }
+      pthread_mutex_unlock(targ->mutex[MUTEX_CART_AT_BAY]);
 
-        this_cart->cart_state = STATE_EXITING;
-        
-        sprintf(message, "C%d: ride done. waiting for passengers to exit\n", this_id);
-        print_timestamped(message);
-        sprintf(message, "C%d: opening doors\n", this_id);
-        print_timestamped(message);
+      sprintf(message, "C%d: ride done. waiting for passengers to exit\n", this_id);
+      print_timestamped(message);
+      sprintf(message, "C%d: opening doors\n", this_id);
+      print_timestamped(message);
+
+      pthread_mutex_lock(targ->mutex[MUTEX_RIDE_FINISH]);
+      this_cart->cart_state = STATE_EXITING;
+      pthread_cond_broadcast(targ->cond[MUTEX_RIDE_FINISH]);
+      pthread_mutex_unlock(targ->mutex[MUTEX_RIDE_FINISH]);
 
         /* Wait for passengers to exit */
-        pthread_mutex_lock(targ->mutex[MUTEX_LEAVE]);
-        while(this_cart->passengers_inside > 0)
-        {
-            pthread_cond_wait(targ->cond[MUTEX_LEAVE], targ->mutex[MUTEX_LEAVE]);
-            pthread_mutex_unlock(targ->mutex[MUTEX_LEAVE]);
-        }
+      pthread_mutex_lock(targ->mutex[MUTEX_LEAVE]);
+      while(this_cart->passengers_inside > 0)
+      {
+          pthread_cond_wait(targ->cond[MUTEX_LEAVE], targ->mutex[MUTEX_LEAVE]);
+      }
+      pthread_mutex_unlock(targ->mutex[MUTEX_LEAVE]);
 
-        sprintf(message, "C%d: closing doors\n", this_id);
-        print_timestamped(message);
+      sprintf(message, "C%d: closing doors\n", this_id);
+      print_timestamped(message);
 
-        this_cart->cart_state = STATE_NOT_ACCEPTING;
-        this_cart->rides_done++;
+      this_cart->cart_state = STATE_NOT_ACCEPTING;
+      this_cart->rides_done++;
     }
 
     sprintf(message, "C%d: ending execution\n", this_id);
     print_timestamped(message);
-    
     free(arg);
+
     return NULL;
 }
 
@@ -221,83 +253,82 @@ void *passenger_routine(void* arg)
     while(targ->cart_head->prev->rides_done < targ->rides_target) 
     {
 
-        /* Check if this passenger is in front of the line */
-        while(*targ->passenger != this_passenger)
-            {passenger_test_stop_cond(targ);}
+      /* Check if this passenger is in front of the line */
+      pthread_mutex_lock(targ->mutex[MUTEX_QUEUE_FRONT]);
+      while(*targ->passenger != this_passenger)
+      {
+        pthread_cond_wait(targ->cond[MUTEX_QUEUE_FRONT], targ->mutex[MUTEX_QUEUE_FRONT]);
+      }
+      pthread_mutex_unlock(targ->mutex[MUTEX_QUEUE_FRONT]);
 
-        /* While cart not accepting */
-        while((*targ->cart)->cart_state != STATE_ACCEPTING || (*targ->cart)->passengers_inside >= targ->passenger_cap)
-            { passenger_test_stop_cond(targ);}
+      /* While cart not accepting */
+      pthread_mutex_lock(targ->mutex[MUTEX_WAIT_FOR_PASSENGERS]);
+      while((*targ->cart)->cart_state != STATE_ACCEPTING || (*targ->cart)->passengers_inside >= targ->passenger_cap)
+      {
+        pthread_cond_wait(targ->cond[MUTEX_WAIT_FOR_PASSENGERS], targ->mutex[MUTEX_WAIT_FOR_PASSENGERS]);
+      }
+      pthread_mutex_unlock(targ->mutex[MUTEX_WAIT_FOR_PASSENGERS]);
 
-        pthread_mutex_lock(targ->mutex[MUTEX_ENTER]);
-        {
-            this_cart = *targ->cart;
-            this_cart->passengers_inside++;
-            *targ->passenger = (*targ->passenger)->next;
-            sprintf(message, "P%d: entered C%d. State: %d\\%d\n",
-                    this_id, this_cart->cart_id, this_cart->passengers_inside, targ->passenger_cap);
-            print_timestamped(message);
-        }
-        pthread_cond_broadcast(targ->cond[MUTEX_ENTER]);
-        pthread_mutex_unlock(targ->mutex[MUTEX_ENTER]);
-        
+      pthread_mutex_lock(targ->mutex[MUTEX_ENTER]);
+      {
+          this_cart = *targ->cart;
+          this_cart->passengers_inside++;
+
+          pthread_mutex_lock(targ->mutex[MUTEX_QUEUE_FRONT]);
+          *targ->passenger = (*targ->passenger)->next;
+          pthread_mutex_unlock(targ->mutex[MUTEX_QUEUE_FRONT]);
+
+          sprintf(message, "P%d: entered C%d. State: %d\\%d\n",
+                  this_id, this_cart->cart_id, this_cart->passengers_inside, targ->passenger_cap);
+          print_timestamped(message);
+      }
+      pthread_cond_broadcast(targ->cond[MUTEX_QUEUE_FRONT]);
+      pthread_cond_broadcast(targ->cond[MUTEX_ENTER]);
+      pthread_mutex_unlock(targ->mutex[MUTEX_ENTER]);
+
         /* Wait until cart gets full */
-        while(this_cart->cart_state != STATE_WAITING_FOR_START &&
-              this_cart->cart_state != STATE_STARTED &&
-              this_cart->cart_state != STATE_RIDING) {}
+      pthread_mutex_lock(targ->mutex[MUTEX_RIDE_START]);
+      while(this_cart->cart_state != STATE_WAITING_FOR_START &&
+            this_cart->cart_state != STATE_STARTED &&
+            this_cart->cart_state != STATE_RIDING) {
+        pthread_cond_wait(targ->cond[MUTEX_RIDE_START], targ->mutex[MUTEX_RIDE_START]);
+      }
+      pthread_mutex_unlock(targ->mutex[MUTEX_RIDE_START]);
 
-        /* If nobody done that, press 'start' */
-        pthread_mutex_lock(targ->mutex[MUTEX_BUTTON]);
-        if(this_cart->cart_state == STATE_WAITING_FOR_START)
-        {
-            sprintf(message, "P%d: pressed 'start' button in C%d\n", this_id, this_cart->cart_id);
-            print_timestamped(message);
-            this_cart->cart_state = STATE_STARTED;
-        }
-        pthread_mutex_unlock(targ->mutex[MUTEX_BUTTON]);
+      /* If nobody done that, press 'start' */
+      pthread_mutex_lock(targ->mutex[MUTEX_BUTTON]);
+      if(this_cart->cart_state == STATE_WAITING_FOR_START)
+      {
+          sprintf(message, "P%d: pressed 'start' button in C%d\n", this_id, this_cart->cart_id);
+          print_timestamped(message);
+          this_cart->cart_state = STATE_STARTED;
+      }
+      pthread_cond_broadcast(targ->cond[MUTEX_WAIT_FOR_BUTTON]);
+      pthread_mutex_unlock(targ->mutex[MUTEX_BUTTON]);
 
-        /* Wait until ride finish */
-        while(this_cart->cart_state != STATE_EXITING)
-            {passenger_test_stop_cond(targ);}
-        
-        pthread_mutex_lock(targ->mutex[MUTEX_LEAVE]);
-        {
-            this_cart->passengers_inside--;
-            sprintf(message, "P%d: left C%d. State: %d\\%d\n",
-                    this_id, this_cart->cart_id, this_cart->passengers_inside, targ->passenger_cap);
-            print_timestamped(message);
-        }
-        pthread_cond_broadcast(targ->cond[MUTEX_LEAVE]);
-        pthread_mutex_unlock(targ->mutex[MUTEX_LEAVE]);
+      /* Wait until ride finish */
+      pthread_mutex_lock(targ->mutex[MUTEX_RIDE_FINISH]);
+      while(this_cart->cart_state != STATE_EXITING)
+      {
+        pthread_cond_wait(targ->cond[MUTEX_RIDE_FINISH], targ->mutex[MUTEX_RIDE_FINISH]);
+      }
+      pthread_mutex_unlock(targ->mutex[MUTEX_RIDE_FINISH]);
+
+      pthread_mutex_lock(targ->mutex[MUTEX_LEAVE]);
+      {
+          this_cart->passengers_inside--;
+          sprintf(message, "P%d: left C%d. State: %d\\%d\n",
+                  this_id, this_cart->cart_id, this_cart->passengers_inside, targ->passenger_cap);
+          print_timestamped(message);
+      }
+      pthread_cond_broadcast(targ->cond[MUTEX_LEAVE]);
+      pthread_mutex_unlock(targ->mutex[MUTEX_LEAVE]);
     }
 
     sprintf(message, "P%d: ending execution\n", this_id);
     print_timestamped(message);
     free(arg);
     return NULL;
-}
-
-void cart_test_stop_cond(T_Arg *targ)
-{
-    if(targ->cart_head->prev->rides_done < targ->rides_target)
-        {return;}
-
-    char message[256];
-    sprintf(message, "C%d: ending execution\n", targ->identifier);
-    print_timestamped(message);
-    free(targ);
-    pthread_exit(NULL);
-}
-void passenger_test_stop_cond(T_Arg *targ)
-{
-    if(targ->cart_head->prev->rides_done < targ->rides_target)
-        {return;}
-
-    char message[256];
-    sprintf(message, "P%d: ending execution\n", targ->identifier);
-    print_timestamped(message);
-    free(targ);
-    pthread_exit(NULL);
 }
 
 /*
